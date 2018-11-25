a4967410cd8f4cbec0d070b9752a2edc7c304fad
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-11-24 17:39:46 -0800

    🎬 updated flask app to render template

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ APP = Flask(__name__)
 @APP.route('/')
 def sample_function():
     sample_word = sample.sample(sample.cumulative_distribution(sample.read_hist("histogram.txt")))
-    return sample_word
+    return render_template('index.html', sample_word=sample_word)
 
-sample_function()
+if __name__ == '__main__':
+    sample_function()
