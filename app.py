e86e781273edc319a69953e7e601399f36fe9105
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-11-25 17:21:59 -0800

    💋 more styling; figuring out button

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sample
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def sample_function():
     sample_sentence = " ".join(sample.num_of_words(4))
     return render_template('index.html', sample_sentence=sample_sentence)
