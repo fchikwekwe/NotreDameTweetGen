b5e09693bacbe0bbdb8bbd5c0ded03fba1f37909
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-11-25 16:26:10 -0800

    🧚🏾‍♀️ styling page

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ APP = Flask(__name__)
 @APP.route('/')
 def sample_function():
     sample_sentence = " ".join(sample.num_of_words(4))
-    print(sample_sentence)
     return render_template('index.html', sample_sentence=sample_sentence)
 
 if __name__ == '__main__':
